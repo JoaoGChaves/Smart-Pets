dc6840756c0990644fcfa3121a83f7163d1aed64
PedroDhalia <pedro.dhalia@gmail.com>
2024-05-06 23:36:00 -0300

    entrega 3

--- clientes/views.py
+++ clientes/views.py
@@ -1,16 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Cliente, Pet
+import re
+from django.core import serializers
+import json
 
-def clientes(request):
-    if request.method=="GET":
-        return render(request, 'clientes.html')
+def clientes(request):  
+    if request.method== "GET":
+        clientes_list=Cliente.objects.all()
+        return render(request, 'clientes.html', {'clientes': clientes_list}) 
     elif request.method=="POST":
         nome=request.POST.get('nome')
-        sobrenome =request.POST.get('sobrenome')
-        email =request.POST.get('email')
-        cpf =request.POST.get('cpf')
-        pets =request.POST.getlist('pet')
-        racas =request.POST.getlist('raca')
-        idades =request.POST.getlist('idade')
+        sobrenome=request.POST.get('sobrenome')
+        email=request.POST.get('email')
+        cpf=request.POST.get('cpf')
+        pets=request.POST.getlist('pet')
+        racas=request.POST.getlist('raca')
+        idades=request.POST.getlist('idade')
+
+        cliente=Cliente.objects.filter(cpf=cpf)
+
+        if cliente.exists():
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email': email, 'pets': zip(pets, racas, idades)})
+        
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, 'pets': zip(pets, racas, idades)})
+        
+        cliente=Cliente(
+            nome= nome,
+            sobrenome= sobrenome,
+            email=email,
+            cpf=cpf,
+        )
+        cliente.save()
+        for carro, raca, idade in zip(pets, racas, idades):
+            pat= Pet(carro=carro, raca=raca, idade=idade, cliente=cliente)
+            pat.save()
+        return HttpResponse('teste')
     
 
+def att_cliente(request):
+    id_cliente=request.POST.get('id_cliente')
+
+    cliente=Cliente.objects.filter(id=id_cliente)
+    pets=Pet.objects.filter(cliente=cliente[0])
+
+    cliente_json=json.loads(serializers.serialize('json', cliente))[0]['fields']
+    pets_json=json.loads(serializers.serialize('json', pets))
+    pets_json=[{'fields': pet['fields'], 'id': pet['pk']} for pet in pets_json]
+    data ={'cliente': cliente_json, 'pets': pets_json}
+    return JsonResponse(data)
--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -6,3 +6,70 @@ function add_pet(){
 
     container.innerHTML += html
 }
+ function exibir_form(tipo){
+    add_cliente=document.getElementById('adicionar-cliente')
+    att_cliente=document.getElementById('att_cliente')
+
+    if(tipo=="1"){
+        att_cliente.style.display ="none"
+        add_cliente.style.display ="block"
+    }else if(tipo=="2"){
+        add_cliente.style.display ="none"
+        att_cliente.style.display ="block"
+    }
+ }
+
+ function dados_cliente(){
+    cliente= document.getElementById('cliente-select')
+    csrf_token= document.querySelector('[name=csrfmiddlewaretoken]').value
+    id_cliente=cliente.value
+
+    data= new FormData()
+    data.append('id_cliente', id_cliente)
+
+    fetch("/clientes/atualiza_cliente/", {
+        method:"POST", 
+        headers:{
+            'X-CSRFToken': csrf_token,
+        },
+        body: data
+
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+        document.getElementById('form-att-cliente').style.display = 'block'
+        nome=document.getElementById('nome')
+        nome.value=data['cliente']['nome']
+
+        sobrenome=document.getElementById('sobrenome')
+        sobrenome.value=data['cliente']['sobrenome']
+
+        email=document.getElementById('email')
+        email.value=data['cliente']['email']
+
+        cpf=document.getElementById('cpf')
+        cpf.value=data['cliente']['cpf']
+
+        div_pets=document.getElementById('pets')
+        div_pets.innerHTML=""
+
+        for(i=0;i<data['pets'].length; i++){
+            console.log(data['pets'][i]['fields']['carro'])
+            div_pets.innerHTML += "<form action='' method''>\
+                <div class='row'>\
+                    <div class='col-md'>\
+                        <input class='form-control' type='text' name='pet' value='"  +data['pets'][i]['fields']['carro']+ "'>\
+                    </div>\
+                    <div class='col-md'>\
+                        <input class='form-control' type='text' name='raca' value='"  +data['pets'][i]['fields']['raca']+ "'>\
+                    </div>\
+                    <div class='col-md'>\
+                        <input class='form-control' type='text' name='idade' value='"  +data['pets'][i]['fields']['idade']+ "'>\
+                    </div>\
+                    <div class='col-md'>\
+                        <input class='btn btn-success' type='submit'>\
+                    </div>\
+                </div><br>"
+        }
+    })
+ }      
